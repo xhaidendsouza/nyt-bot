d9d09b3f7552b1bee786ed21c2a6efa875164bfa
lolguineapig <brendendsouz922@gmail.com>
2025-09-03 18:57:55 -0400

    port update

--- app.py
+++ app.py
@@ -844,5 +844,5 @@ bot.run(os.getenv("TOKEN"))
 def index():
     return render_template("index.html")
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
